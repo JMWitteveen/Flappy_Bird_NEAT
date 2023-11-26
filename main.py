451dd0687f50a6cc1d1741c53071cd1e1fbd6558
JMWitteveen <junior.m.witteveen@gmail.com>
2023-11-26 13:46:08 +0100

    Created Pipe class

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import pygame
 import neat
 import time
 import os
-import random
 
 from bird import Bird
+from pipe import Pipe
+from base import Base
 
 WIN_WIDTH = 550
 WIN_HEIGHT = 800
 
-PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
+
 BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
 BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
 
---
+++ pipe.py
@@ -0,0 +1,48 @@
+import pygame
+import os
+import random
+PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
+
+class Pipe():
+    GAP_BETWEEN_PIPES = 200
+    X_VELOCITY = 5
+
+    def __init__(self, x):
+        self.x = x
+        self.height = 0
+
+        self.top = 0
+        self.bottom = 0
+        self.PIPE_TOP = pygame.transform.flip(PIPE_IMG, False, True)
+        self.PIPE_BOTTOM = PIPE_IMG
+
+        self.passed = False
+        self.set_height()
+
+    def set_height(self):
+        self.height = random.randrange(50, 450)
+        self.top = self.height - self.PIPE_TOP.get_height
+        self.bottom = self.height + self.GAP_BETWEEN_PIPES
+
+    def move(self):
+        self.x -= self.X_VELOCITY
+    
+    def draw(self, win):
+        win.blit(self.PIPE_TOP, (self.x, self.top))
+        win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
+
+    def collide(self, bird):
+        bird_mask = bird.get_mask()
+        top_mask = pygame.mask.from_surface(self.PIPE_TOP)
+        bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
+
+        top_offset = (self.x - bird.x, self.top - round(bird.y))
+        bottom_offset = (self.x, bird.x, self.bottom - round(bird.y))
+
+        top_point = bird_mask.overlap(top_mask, top_offset)
+        bottom_point = bird_mask.overlap(bottom_mask, bottom_offset)
+    
+        if top_point or bottom_point:
+            return True
+        
+        return False
