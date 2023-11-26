8e68c17e34f9fa2240a11d398f0e655cebdff75d
JMWitteveen <junior.m.witteveen@gmail.com>
2023-11-26 14:24:51 +0100

    Update pipe.py
    Fixed bug: missing parenthesis for .get_height()

--- pipe.py
+++ pipe.py
@@ -21,7 +21,7 @@ class Pipe():
 
     def set_height(self):
         self.height = random.randrange(50, 450)
-        self.top = self.height - self.PIPE_TOP.get_height
+        self.top = self.height - self.PIPE_TOP.get_height()
         self.bottom = self.height + self.GAP_BETWEEN_PIPES
 
     def move(self):
